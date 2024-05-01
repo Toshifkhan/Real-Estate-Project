a31c6ae0d84c2806be14f02b541887382fae9201
Toshif <toshifkhan94.tk@gmail.com>
2024-05-01 15:47:20 +0530

    Create and run server

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <h1 className="text-red-500">Hello toshif</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/sign-In" element={<SignIn />}></Route>
+          <Route path="/sign-Up" element={<SignUp />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
